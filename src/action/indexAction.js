0b28577cd25446b8360401639f5abdde9da89e05
Gaurav Patel <dacomida007@gmail.com>
2019-12-22 13:50:19 +0530

    first commit

---
+++ src/action/indexAction.js
@@ -0,0 +1,2 @@
+import counter1 from './comReducers/test1'
+import counter2 from './comReducers/test2'
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux'
-import appReduce from '../reducer/indexReducer'
-
+import { createStore } from 'redux';
+import appReduce from '../reducer/indexReducer';
+import action1 from '../action/comAction/test1Action';
 let storeApp=createStore(appReduce)
+console.log(storeApp.getState())
+storeApp.dispatch(action1())
 console.log(storeApp.getState())
 export default storeApp;
