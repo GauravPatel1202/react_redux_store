25eecf959b4ba6ec274bf29f15e7616fdf5af119
Gaurav Patel <dacomida007@gmail.com>
2019-12-22 14:29:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import storeApp from '../src/store/store'
-
-function App() {
+import { connect } from 'react-redux'
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      React And Redux 
+      {props.counter1}
     </div>
   );
 }
-
-export default App;
+const mapStateToProps = state => {
+  return state
+};
+export default connect(
+  mapStateToProps,
+  null
+)(App)
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,7 @@
 import { createStore } from 'redux';
 import appReduce from '../reducer/indexReducer';
 import action1 from '../action/comAction/test1Action';
-let storeApp=createStore(appReduce)
-console.log(storeApp.getState())
+let storeApp=createStore(appReduce,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 storeApp.dispatch(action1())
-console.log(storeApp.getState())
 export default storeApp;
